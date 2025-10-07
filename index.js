d0d1792294de0cb88062b4928fcd95e2bed2660e
tom045388-maker <tom045388@gmail.com>
2025-10-07 15:35:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1. Create two variables, firstCard and secondCard. 
+// Set their values to a random number between 2-11
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+
+firstCard = 3;
+secondCard = 5;
+sum = firstCard + secondCard;
+console.log(sum)
